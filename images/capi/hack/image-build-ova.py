7d573e59b5aaceb3364b68b28a1fa4ca13836867
Peri Thompson <perit@vmware.com>
2020-09-26 18:04:29 +0100

    Add Windows capv support
    Signed-off-by: Peri Thompson <perit@vmware.com>

--- images/capi/hack/image-build-ova.py
+++ images/capi/hack/image-build-ova.py
@@ -107,6 +107,7 @@ def main():
                  "centos7-64": {"id": "107", "version": "7", "type": "centos7-64"},
                  "rhel7-64": {"id": "80", "version": "7", "type": "rhel7_64guest"},
                  "ubuntu-64": {"id": "94", "version": "", "type": "ubuntu-64"}}
+                 "Windows2019Server-64": {"id": "112", "version": "", "type": "windows2019srv_64Guest"}
 
     # Create the OVF file.
     data = {
